29b08e9ebf61e6be33a8bdd17045694ce61b22be
Kauê Riki Kakinoana <126582273+kaueriki@users.noreply.github.com>
2024-11-22 16:07:45 -0300

    Aula 4

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ import express from "express";
 import routes from "./src/routes/postsRoutes.js";
 
 const app = express();
-
-routes(app);
+routes(app)
 
 app.listen(3000, () => {
     console.log("Servidor escutando...");
 });
+
+
--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,7 +1,31 @@
-import { getTodosPosts } from "../models/postsModels.js";
+import { getTodosPosts, criarPost } from "../models/postsModel.js";
+import fs from "fs";
 
-export async function listarPosts (req, res)
-{
-    const posts =  await getTodosPosts();
-    res.status(200).json(posts);
+export async function listarPosts(req, res) {
+        const posts = await getTodosPosts();
+        res.status(200).json(posts);
+}
+
+export async function cadastrarPosts(req, res) {
+    const novoPosts = req.body;
+    try {
+        const postCriado = await criarPost(novoPosts);
+        res.status(200).json(postCriado);
+    } catch(erro) {
+        console.error(erro.message);
+        res.status(500).json({"Erro": "Falha na requisição"});
+    }
+}
+
+export async function uploadImagem(req, res) {
+    const novoPosts = req.body;
+    try {
+        const postCriado = await criarPost(novoPosts);
+        const imagemAtualizada = `uploads/${postCriado.insertedId}.png`;
+        fs.renameSync(req.file.path, imagemAtualizada)
+        res.status(200).json(postCriado);
+    } catch(erro) {
+        console.error(erro.message);
+        res.status(500).json({"Erro": "Falha na requisição"});
+    }
 }
